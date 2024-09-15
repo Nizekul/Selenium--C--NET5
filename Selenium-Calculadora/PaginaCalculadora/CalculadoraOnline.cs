87aa5f16ed1c485a1bd7b1314268db6f06b0254c
Nizekul <lg.lajedo2@gmail.com>
2024-09-15 16:34:18 -0300

    criando projeto

---
+++ Selenium-Calculadora/PaginaCalculadora/CalculadoraOnline.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Extensions.Configuration;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumCalculadora.PaginaCalculadora
+{
+    public class CalculadoraOnline
+    {
+        private readonly IConfiguration _configuration;
+        private IWebDriver _driver;
+
+        public CalculadoraOnline()
+        {
+            //_configuration = configuration;
+
+            var chromeOptions = new ChromeOptions();
+            chromeOptions.AddArgument("--headless");
+
+            _driver = new ChromeDriver(
+                "Drivers/ChromeDriver.exe",
+                chromeOptions);
+        }
+
+        public void CarregarPagina()
+        {
+            _driver.Manage().Timeouts().PageLoad =
+                TimeSpan.FromSeconds(60);
+            _driver.Navigate().GoToUrl(
+                "https://www.calculadoraonline.com.br/basica"
+                );
+        }
+
+        public void ObterValores()
+        {
+            var vegetable = _driver.FindElement(By.ClassName("bt bt1 cnum"));
+
+            var rowsCotacoes = _driver
+                .FindElement(By.Id("b1")
+                );
+
+        }
+
+        public void Fechar()
+        {
+            _driver.Quit();
+            _driver = null;
+        }
+    }
+}
--- Selenium-Calculadora/Program.cs
+++ Selenium-Calculadora/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using SeleniumCalculadora.PaginaCalculadora;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        CalculadoraOnline teste = new CalculadoraOnline();
+
+        teste.ObterValores();
+    }
+}

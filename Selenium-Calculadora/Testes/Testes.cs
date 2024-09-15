5d511d400fe8fdd790c4ba54dea2f0cd0061a4bd
Marina Flores <mlbtf@discente.ifpe.edu.br>
2024-09-15 19:39:37 -0300

    refatorando teste de soma

--- Selenium-Calculadora/Testes/Testes.cs
+++ Selenium-Calculadora/Testes/Testes.cs
@@ -1,55 +1,59 @@
 ﻿using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
-using Selenium_Calculadora.Xml; 
+using Selenium_Calculadora.Xml;
 
 namespace Selenium_Calculadora.Testes
 {
-    [TestFixture] 
+    [TestFixture]
     public class Testes
     {
         private IWebDriver _driver;
         private XMLHelper _XMLHelper;
+        private IWebElement _input;
+        private IWebElement _btnResult;
+        private IWebElement _divResultado;
 
         [SetUp]
         public void Setup()
         {
             _driver = new ChromeDriver();
-            _driver.Navigate().GoToUrl("https://www.calculadoraonline.com.br/basica"); 
+            _driver.Navigate().GoToUrl("https://www.calculadoraonline.com.br/basica");
 
             _XMLHelper = new XMLHelper();
+            _input = _driver.FindElement(By.Id("TIExp"));
+            _btnResult = _driver.FindElement(By.Id("b27"));
+            _divResultado = _driver.FindElement(By.Id("TIExp"));
         }
 
-        [Test]
-        public void TesteProcedimentos()
+        [TestCase("Soma")]
+        public void TestarOperacao(string operacao)
         {
             IWebElement input = _driver.FindElement(By.Id("TIExp"));
 
             var procedimentos = _XMLHelper.LerProcedimentos();
 
-            foreach (var procedimento in procedimentos)
+            foreach (var item in procedimentos.Where(p => p.Nome == operacao))
             {
-                foreach (var caso in procedimento.Casos)
-                {
-                    var entrada1 = caso.Entrada1;
-                    var entrada2 = caso.Entrada2;
-
-                    var entrada = CriarEntrada(procedimento.Nome, entrada1, entrada2);
-
-                    input.SendKeys($"{entrada}");                   
-
-                    IWebElement btnResult = _driver.FindElement(By.Id("b27"));
-                    btnResult.Click();
-
-                    IWebElement divResultado = _driver.FindElement(By.Id("TIExp"));
-                    string resultado = divResultado.GetAttribute("value");
-
-                    Console.WriteLine("Resultado da operação: " + resultado);
-                }
+                foreach (var caso in item.Casos)
+                    TestarCaso(operacao, caso.Entrada1, caso.Entrada2, caso.ResultadoEsperado);                
             }
         }
 
-        private string CriarEntrada(string operacao, string entrada1, string entrada2)
+        private void TestarCaso(string operacao, string entrada1, string entrada2, string resultadoEsperado)
+        {
+            _input.Clear();
+            _input.SendKeys(CriarEntrada(operacao, entrada1, entrada2));
+            _btnResult.Click();
+
+            var resultado = _divResultado.GetAttribute("value");
+            Console.WriteLine($"Resultado da operação {operacao}: {resultado}");
+            Assert.That(resultado, 
+                        Is.EqualTo(resultadoEsperado.ToString()), 
+                        $"Erro ao realizar a operação {operacao} com a entrada {entrada1} + {entrada2}. Resultado esperado: {resultadoEsperado}, Resultado obtido: {resultado}");
+        }
+
+        private static string CriarEntrada(string operacao, string entrada1, string entrada2)
         {
             return operacao switch
             {
